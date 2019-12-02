ed4701a4ed3fb6c7f7f1f47e2430fe5ef4c99f5e
Kathleen McDaniel <kathleen_mc.daniel@yahoo.com>
2019-12-02 16:15:57 -0500

    style: added prettier file

--- client/components/map-screen.js
+++ client/components/map-screen.js
@@ -1,32 +1,32 @@
-import React, { Component } from "react";
-import { View, Button, Text, SafeAreaView } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import React, {Component} from 'react'
+import {View, Button, Text, SafeAreaView} from 'react-native'
+import MapView, {Marker} from 'react-native-maps'
 //import * as Permissions from 'expo-permissions'
 //import * as Location from 'expo-location'
-import { connect } from "react-redux";
-import { PermissionsAndroid } from "react-native";
+import {connect} from 'react-redux'
+import {PermissionsAndroid} from 'react-native'
 //------------------------------------------------------------------
 import {
   fetchAllHuntLocations,
   fetchVisitedHuntLocation,
   fetchDroppingHuntLocations
-} from "../store/huntLocations";
-import { coordDist } from "../../coordinate-logic";
-import { styles } from "../styles";
+} from '../store/huntLocations'
+import {coordDist} from '../../coordinate-logic'
+import {styles} from '../styles'
 //------------------------------------------------------------------
 //classifies if component is currently mounted
-let mounted = true;
+let mounted = true
 //determines default zoom for map
-const LATITUDE_DELTA = 0.00922;
-const LONGITUDE_DELTA = 0.00421;
+const LATITUDE_DELTA = 0.00922
+const LONGITUDE_DELTA = 0.00421
 //------------------------------------------------------------------
 class MapScreen extends Component {
   //------------------------------------------------------------------
   static navigationOptions = {
     headerLeft: null
-  };
+  }
   constructor() {
-    super();
+    super()
     this.state = {
       latitude: 0,
       longitude: 0,
@@ -35,62 +35,62 @@ class MapScreen extends Component {
       level: 0,
       score: 0,
       won: false
-    };
-    this.handleFound = this.handleFound.bind(this);
-    this.updatePosition = this.updatePosition.bind(this);
-    this.backToStart = this.backToStart.bind(this);
-    this.requestLocationPermission = this.requestLocationPermission.bind(this);
+    }
+    this.handleFound = this.handleFound.bind(this)
+    this.updatePosition = this.updatePosition.bind(this)
+    this.backToStart = this.backToStart.bind(this)
+    this.requestLocationPermission = this.requestLocationPermission.bind(this)
   }
   //----------------FUNCTIONS--------------------------------------
   async componentDidMount() {
-    mounted = true;
-    this.requestLocationPermission();
+    mounted = true
+    this.requestLocationPermission()
     //-------------------HUNTS---------------------------------------------
-    await this.props.fetchHuntLocations(this.props.user.id);
+    await this.props.fetchHuntLocations(this.props.user.id)
     let initialScore = this.props.huntLocations.filter(
       loc => loc.huntLocation.visited
-    ).length;
+    ).length
 
     this.setState({
       score: initialScore,
       level: initialScore
-    });
+    })
   }
   //------------------------------------------------------------------
   async handleFound(targetLat, targetLong) {
     //variable declarations
-    let huntLocs = this.props.huntLocations;
-    let huntLocId = huntLocs[this.state.level].huntLocation.locationId;
+    let huntLocs = this.props.huntLocations
+    let huntLocId = huntLocs[this.state.level].huntLocation.locationId
     let withinDistance =
       coordDist(
         this.state.latitude,
         this.state.longitude,
         targetLat,
         targetLong
-      ) < 100;
-    let levelsToComplete = this.props.huntLocations.length - this.state.level;
+      ) < 100
+    let levelsToComplete = this.props.huntLocations.length - this.state.level
 
     //conditional logic
     if (withinDistance) {
-      levelsToComplete--;
+      levelsToComplete--
       //update visited to "true" for this location
-      await this.props.fetchVisitLocation(this.props.user.id, huntLocId);
+      await this.props.fetchVisitLocation(this.props.user.id, huntLocId)
       //increment score
       this.setState(prevState => {
-        return { score: prevState.score + 1 };
-      });
+        return {score: prevState.score + 1}
+      })
 
       if (levelsToComplete > 0) {
         //Increment next level
         this.setState(prevState => {
-          return { level: prevState.level + 1 };
-        });
+          return {level: prevState.level + 1}
+        })
       } else {
-        this.setState({ won: true });
-        await this.props.fetchDropLocations(this.props.user.id);
+        this.setState({won: true})
+        await this.props.fetchDropLocations(this.props.user.id)
         setTimeout(() => {
-          this.props.navigate("StartScreen");
-        }, 4000);
+          this.props.navigate('StartScreen')
+        }, 4000)
       }
     }
   }
@@ -101,21 +101,21 @@ class MapScreen extends Component {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         {
-          title: "AdventurAR App Location Permission",
+          title: 'AdventurAR App Location Permission',
           message:
-            "AdventurAR App needs access to your location to run the scavenger hunts.",
-          buttonNeutral: "Ask Me Later",
-          buttonNegative: "Cancel",
-          buttonPositive: "OK"
+            'AdventurAR App needs access to your location to run the scavenger hunts.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK'
         }
-      );
-      console.log("Permission status: ", granted);
+      )
+      console.log('Permission status: ', granted)
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         //-------SET LOCATION TRACKING------------------------------------------
-        this.locationTracking = setInterval(this.updatePosition, 2000);
+        this.locationTracking = setInterval(this.updatePosition, 2000)
       }
     } catch (err) {
-      console.warn(err);
+      console.warn(err)
     }
   }
   //------------------------------------------------------------------
@@ -126,52 +126,55 @@ class MapScreen extends Component {
           this.setState({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-          });
+          })
         }
       },
       error => {
-        console.log("update position error");
-        console.log("error: ", error);
+        console.log('update position error')
+        console.log('error: ', error)
       },
-      { enableHighAccuracy: true, timeout: 2000, maximumAge: 0 }
-    );
+      {enableHighAccuracy: true, timeout: 2000, maximumAge: 0}
+    )
   }
   //------------------------------------------------------------------
   backToStart() {
-    this.props.navigate("StartScreen");
+    this.props.navigate('StartScreen')
   }
   //------------------------------------------------------------------
   componentWillUnmount() {
-    mounted = false;
-    clearInterval(this.locationTracking);
+    mounted = false
+    clearInterval(this.locationTracking)
   }
   //------------------------------------------------------------------
   render() {
-    let huntMarker = this.props.huntLocations[this.state.score];
+    let huntMarker = this.props.huntLocations[this.state.score]
     let huntMarkers = this.props.huntLocations
     let userLoc = {
       latitude: this.state.latitude,
       longitude: this.state.longitude
-    };
+    }
     let huntMarkerCoords = {
       latitude: parseFloat(huntMarker.latitude),
       longitude: parseFloat(huntMarker.longitude)
     }
-    let level = this.state.level;
+    let level = this.state.level
     return (
       <SafeAreaView style={styles.container}>
-        <View style={{ flex: 1 }}>
+        <View style={{flex: 1}}>
           <MapView style={styles.mapStyle} region={this.state}>
             {/* Current user location marker */}
             <Marker coordinate={userLoc}>
               <View style={styles.userLocMarker} />
             </Marker>
             {/* Testing database hunt location marker */}
-            {!huntMarker || coordDist(userLoc.latitude, userLoc.longitude, huntMarkerCoords.latitude, huntMarkerCoords.longitude) > 100 ? null : (
-              <Marker
-                key={huntMarker.id}
-                coordinate={huntMarkerCoords}
-              >
+            {!huntMarker ||
+            coordDist(
+              userLoc.latitude,
+              userLoc.longitude,
+              huntMarkerCoords.latitude,
+              huntMarkerCoords.longitude
+            ) > 100 ? null : (
+              <Marker key={huntMarker.id} coordinate={huntMarkerCoords}>
                 <View style={styles.huntLocMarker} />
               </Marker>
             )}
@@ -196,7 +199,7 @@ class MapScreen extends Component {
             <Text>{huntMarkers[level].riddle}</Text>
             {/* TESTING PARAMETERS */}
             <Text>
-              TARGET: {huntMarkers[level].latitude} :{" "}
+              TARGET: {huntMarkers[level].latitude} :{' '}
               {huntMarkers[level].longitude}
             </Text>
             <Text>
@@ -235,7 +238,7 @@ class MapScreen extends Component {
           </View>
         )}
       </SafeAreaView>
-    );
+    )
   }
 }
 //------------------------------------------------------------------
@@ -244,8 +247,8 @@ const mapStateToProps = (state, ownProps) => {
     huntLocations: state.huntLocations,
     user: state.user,
     navigate: ownProps.navigation.navigate
-  };
-};
+  }
+}
 //------------------------------------------------------------------
 const mapDispatchToProps = dispatch => {
   return {
@@ -253,8 +256,8 @@ const mapDispatchToProps = dispatch => {
     fetchVisitLocation: (userId, locationId) =>
       dispatch(fetchVisitedHuntLocation(userId, locationId)),
     fetchDropLocations: userId => dispatch(fetchDroppingHuntLocations(userId))
-  };
-};
+  }
+}
 //------------------------------------------------------------------
 
-export default connect(mapStateToProps, mapDispatchToProps)(MapScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(MapScreen)
